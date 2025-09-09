10cbc3a6c0bd6cbd6cac6c63e334e5256efde36d
Sarah <galorin@gmail.com>
2025-09-09 13:28:55 +0100

    Moving to TypeScript

--- application/src/frontend/src/utils/helpers.js
+++
@@ -1,4 +0,0 @@
-// src/utils/helpers.js
-export const findIngredientByName = (ingredients, ingredientName) => {
-  return ingredients.find(ing => ing.Name.toLowerCase() === ingredientName.toLowerCase());
-};
